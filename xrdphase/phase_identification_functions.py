165187f04a2fc2a562023abf8a59501f8ea460c5
Student <student@LPS-153291.local>
2019-07-01 16:47:58 -0400

    Modified functions to graph correct model

--- xrdphase/phase_identification_functions.py
+++ xrdphase/phase_identification_functions.py
@@ -53,48 +53,6 @@ def convert_tth_to_q(tth, lam=1.5406):
     return q
 
 
-def plot_background_and_structure(models, modelIndx, qcut, iqcut,
-                                  plot_this=False):
-    """Cuts the sample data to match where a single model's peaks are,
-    then finds and removes the background. If plot_this is set to True,
-    plots the cut data and the calculated background, then the sample data
-    with no background and the position of the model peaks"""
-    model_num = models[modelIndx]['xrd.Cu']['pattern']
-    x_val_mod = []
-    y_val_mod = []
-    effective_peak_x = []
-    effective_peak_y = []
-
-    for i in range(len(model_num)):
-        x_val_mod.append(convert_tth_to_q(model_num[i][2])*.995)
-        y_val_mod.append(model_num[i][0])
-        # choosing the peaks to use based on where the data starts
-        if x_val_mod[i] >= qcut[0]:
-            effective_peak_x.append(x_val_mod[i])
-            effective_peak_y.append(y_val_mod[i])
-
-    # cut the data again for each model
-    qcutM, iqcutM = cut_data(qcut, iqcut, 0, effective_peak_x[-1]+.1)
-
-    bgd = bgd_func(qcutM, iqcutM[0], iqcutM[-1])
-
-    iqcutNoBgd = np.zeros_like(iqcutM)
-    iqcutNoBgd = iqcutM-bgd
-    for i in range(len(iqcutNoBgd)):
-        if iqcutNoBgd[i] < 0:
-            iqcutNoBgd[i] = 0
-
-    if plot_this:
-        plt.figure()
-        plt.plot(qcutM, iqcutM, label="Data")
-        plt.plot(qcutM, bgd, label="Background")
-        plt.figure()
-        plt.plot(qcutM, iqcutNoBgd, label="Data - No Background")
-        plt.scatter(effective_peak_x, effective_peak_y, label="Model",
-                    marker='.', color='r')
-        plt.legend(loc=0)
-
-
 def identify_phase(models, qcut, iqcut):
     """Identifies the most likely spacegroup symbol and material_id based
     on the sample data and Materials Project models.
@@ -168,46 +126,111 @@ def identify_phase(models, qcut, iqcut):
 
         # cuts out models with way too many peaks
         if len(effective_peak_x) <= 2*len(qcutM[peaks]):
-            plt.figure()
-            plt.plot(qcutM, iqcutNoBgd)
-            plt.scatter(effective_peak_x, effective_peak_y, label="Model",
-                        marker='.', color='r')
-            plt.plot(qcutM, sum_gauss(qcutM, *initial_guess),
-                     label="Initial Guess")
-            plt.title(models[j]['material_id'] + "\t" +
-                      models[j]['spacegroup']['symbol'])
-
-            print("zero count is " + str(zero_count))
-
             try:
                 fit_data, _ = curve_fit(sum_gauss, qcutM, iqcutNoBgd,
                                         p0=initial_guess,
                                         bounds=([low_lims, high_lims]),
                                         maxfev=100)
-                plt.plot(qcutM, iqcutNoBgd, label="Data")
-                plt.plot(qcutM, sum_gauss(qcutM, *fit_data), label="Fit")
-                plt.legend(loc=0)
-
-                print('done fitting the curve')
+                print("A curve was fit for " + models[j]['material_id'])
 
                 residual += abs(iqcutNoBgd-sum_gauss(qcutM, *fit_data))
                 sumRes = sum(residual)+zero_count*1000
-                print("Sum of residual: " + str(sumRes))
                 resSumList.append(sumRes)
-
                 modelList.append(j)
 
             except RuntimeError:
                 residual = 100000+zero_count*1000
-                print("Curve fit probably failed here")
-                print("Bad curve residual: " + str(residual))
+                print("Curve fit failed for " + models[j]['material_id'])
         else:
             print("I think curve fit will fail for " +
                   models[j]['material_id'] +
                   ". There might be too many peaks")
 
     # find index of smallest residual
-    smallIndx = resSumList.index(min(resSumList))
+    fitIndx = resSumList.index(min(resSumList))
     print("I think you're looking at " +
-          models[modelList[smallIndx]]['spacegroup']['symbol'] +
-          ', ' + models[modelList[smallIndx]]['material_id'])
+          models[modelList[fitIndx]]['spacegroup']['symbol'] +
+          ', ' + models[modelList[fitIndx]]['material_id'])
+    return fitIndx, modelList
+
+
+def show_correct_model(models, fitIndx, modelList, qcut, iqcut):
+    """Graphs the correct model based on which one fit the best. Pass in
+       the list of models, fitIndx, the list of models that have graphs,
+       qcut, and iqcut"""
+    x_val_mod = []
+    y_val_mod = []
+    effective_peak_x = []
+    effective_peak_y = []
+
+    model_num = models[modelList[fitIndx]]['xrd.Cu']['pattern']
+
+    for i in range(len(model_num)):
+        x_val_mod.append(convert_tth_to_q(model_num[i][2])*.995)
+        y_val_mod.append(model_num[i][0])
+        if x_val_mod[i] >= qcut[0]:
+            effective_peak_x.append(x_val_mod[i])
+            effective_peak_y.append(y_val_mod[i])
+
+        qcutM, iqcutM = cut_data(qcut, iqcut, 0, effective_peak_x[-1]+.1)
+
+        bgd = bgd_func(qcutM, iqcutM[0], iqcutM[-1])
+
+        iqcutNoBgd = np.zeros_like(iqcutM)
+        iqcutNoBgd = iqcutM-bgd
+        for i in range(len(iqcutNoBgd)):
+            if iqcutNoBgd[i] < 0:
+                iqcutNoBgd[i] = 0
+
+        initial_guess = []
+        low_lims = []
+        high_lims = []
+        zero_count = 0
+        residual = 0
+        for i in range(len(effective_peak_x)):
+            if effective_peak_x[i] >= qcut[2]:
+                x = effective_peak_x[i]
+                # used to see if there's a better peak somewhere else
+                peak_max_indx = 2
+                indx = find_nearest(qcut, x)
+
+                my_max = np.max(iqcutNoBgd[indx-peak_max_indx:
+                                indx+peak_max_indx])
+                my_max_indx = find_nearest(iqcutNoBgd, my_max)
+
+                initial_guess.append(qcut[my_max_indx])  # cen
+                initial_guess.append(.04)  # wid
+                initial_guess.append(iqcutNoBgd[my_max_indx])  # amp
+                if iqcutNoBgd[my_max_indx] == 0:
+                    # keeping a count of all the areas where amplitude is 0
+                    zero_count += 1
+
+                low_lims.append(qcut[my_max_indx]-.03)  # cen
+                low_lims.append(.0002)  # wid
+                low_lims.append(0)  # amp
+
+                high_lims.append(qcut[my_max_indx]+.03)  # cen
+                high_lims.append(.06)  # wid
+                high_lims.append(max(iqcutNoBgd))  # amp
+
+        try:
+            fit_data, _ = curve_fit(sum_gauss, qcutM, iqcutNoBgd,
+                                    p0=initial_guess,
+                                    bounds=([low_lims, high_lims]),
+                                    maxfev=100)
+            plt.figure()
+            plt.title(models[modelList[fitIndx]]['material_id'] + "\t" +
+                      models[modelList[fitIndx]]['spacegroup']['symbol'])
+            plt.plot(qcutM, iqcutNoBgd, label="Data")
+            plt.plot(qcutM, sum_gauss(qcutM, *fit_data), label="Fit")
+            plt.scatter(effective_peak_x, effective_peak_y, label="Model",
+                        marker='.', color='r')
+            plt.legend(loc=0)
+
+            residual += abs(iqcutNoBgd-sum_gauss(qcutM, *fit_data))
+            sumRes = sum(residual)+zero_count*1000
+            print("Sum of residual: " + str(sumRes))
+
+        except RuntimeError:
+            residual = 100000+zero_count*1000
+            print("There weren't any fits")
