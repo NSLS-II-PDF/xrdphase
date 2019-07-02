b168f0c81241b80b64a3cfcd31fe510bc329a040
AbbyGi <abbygi09@gmail.com>
2019-07-02 13:41:20 -0400

    Fixed flake8 formatting issues

--- xrdphase/phase_id_script.py
+++ xrdphase/phase_id_script.py
@@ -1,6 +1,8 @@
 # uses python fire for command line interface
-from phase_identification_functions import *
+from phase_identification_functions import get_structures, read_data
+from phase_identification_functions import identify_phase, show_correct_model
 import fire
+
 
 class PhaseIdentification:
     def find_phase(self, apiKey, elementList, fileName, cutDataStart):
@@ -9,8 +11,10 @@ class PhaseIdentification:
         fitIndx, modelList = identify_phase(models, qcut, iqcut)
         show_correct_model(models, fitIndx, modelList, qcut, iqcut)
 
+
 def main():
     fire.Fire(PhaseIdentification)
+
 
 if __name__ == '__main__':
     main()
