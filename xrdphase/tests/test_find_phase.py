fe9db34cae1f5a9801cca70392eba58f6babed0d
AbbyGi <abbygi09@gmail.com>
2019-07-03 13:44:19 -0400

    Fixed the test function's data and model paths

--- xrdphase/tests/test_find_phase.py
+++ xrdphase/tests/test_find_phase.py
@@ -10,7 +10,7 @@ def test_read_data():
     Check that the first element of qcut is greater than startCut and the last
     element of qcut is less than 20
     """
-    qcut, iqcut = piClass._split_read_data(fileName='./xrdphase/tests/test_data/Ni.chi',
+    qcut, iqcut = piClass._split_read_data(fileName='./tests/test_data/Ni.chi',
                                            cutDataStart=2)
     assert qcut[0] >= 2, "Data is being cut wrong"
     assert qcut[-1] <= 20, "Data is being cut wrong"
@@ -18,9 +18,9 @@ def test_read_data():
 
 def test_identify_phase():
     """Check that model_name is the correct model"""
-    with open('./xrdphase/tests/test_data/models.json') as json_file:
+    with open('./tests/test_data/models.json') as json_file:
         models = json.load(json_file)
-    qcut, iqcut = piClass._split_read_data(fileName='./xrdphase/tests/test_data/Ni.chi',
+    qcut, iqcut = piClass._split_read_data(fileName='./tests/test_data/Ni.chi',
                                            cutDataStart=2)
     fitIndx, modelList, model_name = piClass._split_identify_phase(models,
                                                                    qcut, iqcut)
