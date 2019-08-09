1e3e01aaed3025d8dc77ace5fe148912ef402dac
AbbyGi <abbygi09@gmail.com>
2019-08-09 14:46:22 -0400

    Put imports in correct spot

--- xrdphase/__init__.py
+++ xrdphase/__init__.py
@@ -1,10 +1,8 @@
-
+from .phase_id_functions import get_structures, read_data
+from .phase_id_functions import identify_phase, show_correct_model
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
-
-from .phase_id_functions import get_structures, read_data
-from .phase_id_functions import identify_phase, show_correct_model
 
 
 class PhaseIdentification:
